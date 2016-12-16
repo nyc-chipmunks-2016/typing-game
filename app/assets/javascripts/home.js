0a32022d3cf5fcd26b57735eb29a2ae4b2c60aeb
Allain Dollete <allain.dollete@gmail.com>
2016-12-16 13:34:20 -0500

    Create drop down menus for login and register

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,6 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+// Prevent closing the sign in form on click
+$("#dropdown-login").click(function(e) {
+   e.stopPropagation();
+});
