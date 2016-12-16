763a45fe4c4d69b65569063d5bfb00ee6d11fd7a
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-15 20:06:31 -0500

    update users model and migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,8 @@ class User < ActiveRecord::Base
                 uniqueness: true,
                 length: {minimum: 5, maximum: 25}
 
-    validates :password_digest, presence: true,
-              uniqueness: {case_sensitive: false},
-              length: {minimum: 6}
+    validates :password_digest, presence: true,uniqueness: {case_sensitive: false},length: {minimum: 6}
     VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length:{maximum: 105},
-                uniqueness: true,
-                format: {with: VALID_EMAIL_REGEX}
+    validates :email, presence: true, length:{maximum: 105},uniqueness: true,format: {with: VALID_EMAIL_REGEX}
 
 end
--- db/migrate/20161216004539_create_users.rb
+++ db/migrate/20161216004539_create_users.rb
@@ -1,9 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false
-      t.string :password_digest, null: false
-      t.string :email, null: false
+      t.string :username
+      t.string :password_digest
+      t.string :email
+
+      t.timestamps(null:false)
     end
   end
 end
