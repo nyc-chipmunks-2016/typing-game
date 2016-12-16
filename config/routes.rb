cf375c4c12c3e2afda759d20538792ae8b92fe6a
Allain Dollete <allain.dollete@gmail.com>
2016-12-16 11:18:14 -0500

    Add exceptions to games route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  resources :games, except: :edit
+  resources :games, except: [:edit, :index, :show, :update]
 
   root to: "home#index"
 end
