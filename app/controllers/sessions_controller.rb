92c53f9af7fd6e5e271ffa295d35ba43ebe2bdfc
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-16 17:09:48 -0500

    modify login and register page, change txt to password type

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_username(params[:session][:username])
+    user = User.find_by_username(params[:session][:username])
 
-    if @user && user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      redirect_to user_path(@user)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
     else
       @errors = ["The username and password you entered do not match."]
 
