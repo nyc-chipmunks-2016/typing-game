26c2926869dd8ecc14d2c90135c51f45c3f819e5
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-16 09:55:41 -0500

    update user migration by adding null false

--- db/migrate/20161216022926_create_users.rb
+++ db/migrate/20161216022926_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :password_digest
+      t.string :username, null: false
+      t.string :password_digest, null: false
       t.string :email
 
       t.timestamps(null:false)
