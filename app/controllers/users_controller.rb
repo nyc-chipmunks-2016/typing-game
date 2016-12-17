fcef4e4515015db8232e391965a98888bd567a18
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-17 11:31:14 -0500

    users can successfully edit their accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+     @user = User.find(params[:id])
+  end
+
+   def update
+      @user = User.find(params[:id])
+       if @user.update(user_params)
+           redirect_to root_path
+       else
+           render 'edit'
+       end
+   end
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
