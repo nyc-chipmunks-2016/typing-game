52cad364cdd763649ede2af69017aeb102512c50
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-17 14:26:28 -0500

    change default pic on user profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    gravatar_url = "http://www.aspirehire.co.uk/aspirehire-co-uk/_img/profile.svg"
     image_tag(gravatar_url, alt: user.username, class: "img-circle")
   end
 end
