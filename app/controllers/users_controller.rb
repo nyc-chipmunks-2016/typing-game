49b47dd86fa49b551549fb2e429b231709a9f290
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-16 21:23:55 -0500

    format the user page, still editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,13 +9,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
     else
       @errors = @user.errors.full_messages
-
     end
   end
 
