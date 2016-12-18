34f012245de6b990df08f3aa70b356365eb7d378
Allain Dollete <allain.dollete@gmail.com>
2016-12-18 15:16:54 -0500

    Correct error handling for login

--- app/assets/javascripts/components/sessions.js
+++ app/assets/javascripts/components/sessions.js
@@ -1,8 +1,32 @@
 $( document ).ready(function() {
-  $("form#login_form").on("submit", function() {
-    if($("ul.error_login li").size() === 0) {
-      $("ul.error_login").append($("<li />").html("The username and password you entered do not match."));
-    }
+  $("form#login_form").on("submit", function(event) {
+    event.preventDefault();
+    var $form = $(this);
+    var method = $form.attr("method");
+    var url = $form.attr("action");
+    var data = $form.serialize();
+
+    $.ajax({
+      url: url,
+      method: method,
+      data: data
+    })
+    .done(function(response) {
+      window.location.href = response.user_link;
+    })
+    .fail(function() {
+      if($("ul.error_login li").size() === 0) {
+        $("ul.error_login").append($("<li />").html("The username and password you entered do not match."));
+      }
+    });
+
+    // var request = new XMLHttpRequest();
+
+
+  });
+
+  $("button#login_cancel").on("click", function(){
+    $("ul.error_login li").remove();
   });
 
 });
