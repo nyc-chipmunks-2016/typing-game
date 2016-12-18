afb3ba23f9df14bba6e0c045c0d5621d595a1b8c
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-18 13:33:49 -0500

    fix tabbing issues

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 RSpec.describe UsersController, :type => :controller do
 	let(:valid_session) do
-		  { user_id: 1 }
+		{ user_id: 1 }
 	end
 
 	let(:valid_attributes) do
-		{ email: "arsy209@gmail.com", username: "arsy21",password: "123456",}
+		{ email: "arsy209@gmail.com", username: "arsy21",password: "123456"}
 	end
 
 	let(:invalid_attributes) do
@@ -18,29 +18,29 @@ RSpec.describe UsersController, :type => :controller do
 
 describe "POST #create" do
   it " should create a new user" do
-      process :create, params: {user: valid_attributes}
-      expect(assigns(:user)).to eq(User.last)
+    process :create, params: {user: valid_attributes}
+    expect(assigns(:user)).to eq(User.last)
   end
 
   it "returns http success" do
-      post :create, params: {user: valid_attributes}
-      expect(response).to have_http_status(302)
+    post :create, params: {user: valid_attributes}
+    expect(response).to have_http_status(302)
   end
 
   context "When valid information is not put in" do
     it "does not create the user and returns nil" do
-         post :create, params: { user: invalid_attributes}
-         expect(User.find_by_username(valid_attributes[:username])).to be_nil
+      post :create, params: { user: invalid_attributes}
+      expect(User.find_by_username(valid_attributes[:username])).to be_nil
     end
   end
 
   describe "GET #show" do
     it "returns http success" do
-        user = User.create!(valid_attributes)
-        get :show, params: { id: user.id}
-        expect(response).to have_http_status(:success)
-        end
-        after { puts "Completed"}
+      user = User.create!(valid_attributes)
+      get :show, params: { id: user.id}
+      expect(response).to have_http_status(:success)
       end
+      after { puts "Completed"}
     end
+  end
 end
