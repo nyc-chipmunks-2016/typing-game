81bdb20395313b1c5e2f16a12c4d253d85db42c2
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-18 11:36:29 -0500

    write tests for home controller and user controller, all tests pass

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,46 @@
-require 'rails_helper'
+require "rails_helper"
+RSpec.describe UsersController, :type => :controller do
+	let(:valid_session) do
+		  { user_id: 1 }
+	end
 
-RSpec.describe UsersController, type: :controller do
+	let(:valid_attributes) do
+		{ email: "arsy209@gmail.com", username: "arsy21",password: "123456",}
+	end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
+	let(:invalid_attributes) do
+		{ username: nil }
+	end
+
+  it " Should allow a  uer to be created with valid attributes" do
+    newuser = User.create! valid_attributes
   end
 
+describe "POST #create" do
+  it " should create a new user" do
+      post :create, {user: valid_attributes}
+      expect(assigns(:user)).to eq(User.last)
+  end
+
+  it "returns http success" do
+      post :create, {user: valid_attributes}
+      expect(response).to have_http_status(302)
+  end
+
+  context "When valid information is not put in" do
+    it "does not create the user and returns nil" do
+         post :create, { user: invalid_attributes }, valid_session
+         expect(User.find_by_username(valid_attributes[:username])).to be_nil
+    end
+end
+
+describe "GET #show" do
+  it "returns http success" do
+      user = User.create!(valid_attributes)
+      get :show, id: user.id
+      expect(response).to have_http_status(:success)
+      end
+      after { puts "Completed"}
+    end
+  end
 end
