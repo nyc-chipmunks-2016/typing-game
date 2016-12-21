67d52bb5fbd52684b4dbb4c8a6f474fe228466d4
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-21 14:43:03 -0500

    change user default icon from mushroom to volcano icon

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :games
 
-  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" ,:small  => "200x200>" }, default_url: "medium/default.png"
+  has_attached_file :avatar, styles: { medium: "200x200>", thumb: "100x100>" ,:small  => "200x200>" }, default_url: "medium/volcanoIcon.png"
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
