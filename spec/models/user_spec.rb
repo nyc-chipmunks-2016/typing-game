5c008279e76caebe629dc6450db7ee6b3ba2b7d9
Arshdeep Singh <singh_arsy@yahoo.com>
2016-12-21 10:31:54 -0500

    create user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe "User Model" do
+    it "requires presences of an username" do
+      user = User.new(username: "")
+      user.valid?
+      expect(user.errors[:username].any?).to eq(true)
+    end
 
+    it "requires presences of an email" do
+      user = User.new(email: "")
+      user.valid?
+      expect(user.errors[:email].any?).to eq(true)
+    end
+
+    it "requires a unique email address" do
+      user_1 = User.create(username: "arsy21", email: "arsy11@gmail.com",
+      password: "password")
+      user_2 = User.new(username: "arsh24", email: "arsy11@gmail.com",
+      password: "password1")
+      user_2.valid?
+      expect(user_2.errors[:email].any?).to eq(true)
+    end
+
+    it "requires presences of an password" do
+      user = User.new(password: "")
+      user.valid?
+      expect(user.errors[:password].any?).to eq(true)
+    end
+
+    it "automatically encrypts the password using Bycrypt" do
+    user = User.new(password: "password")
+    expect(user.password_digest.present?).to eq(true)
+    end
+  end
 end
